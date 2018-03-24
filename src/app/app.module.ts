f44537b064170f184d689bc52c3bf52057b65078
edumoritz <eduardo.moritz@hotmail.com>
2018-03-24 11:40:33 -0300

    modulo routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 
 @NgModule({
@@ -10,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
