14d4a16a4753f67d41bca9e3454c508177b73727
JHONATAN GRYMUZA ZANATTA <JGZANATTA@univel.univel.br>
2018-04-24 21:36:54 -0300

    criado modal para regraCampo cadastro

---
+++ src/app/features/regra-campo/cadastro/regra-campo-cadastro-modal.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+import { ModalSuperComponent } from '../../../components/modal/modal-super.component';
+import { ModalService } from '../../../components/modal/modal.service';
+import { AModuleService } from '../../../services-abstract/modulo.service';
+import { RegraCampo } from '../../../models/funcionalidade/regra-campo.model';
+
+
+export interface IRegraCampoCadastroModal {
+  regraCampo: RegraCampo;
+}
+
+@Component({
+  selector: 'help-bot-regra-campo-cadastro-modal',
+  templateUrl: './regra-campo-cadastro-modal.component.html'
+})
+export class RegraCampoCadastroModalComponent
+extends ModalSuperComponent<IRegraCampoCadastroModal, void>
+implements IRegraCampoCadastroModal {
+
+  public regraCampo: RegraCampo;
+
+  constructor(
+    public modalService: ModalService,
+    private moduleService: AModuleService
+  ) {
+    super(modalService);
+  }
+
+  public cancelar() {
+    this.modalService.fecharModal(this);
+  }
+
+  public salvar(): void {
+    let acao: Observable<RegraCampo>;
+
+    if (this.regraCampo.id) {
+      acao = this.moduleService.put(this.regraCampo);
+    } else {
+      acao = this.moduleService.post(this.regraCampo);
+    }
+
+    acao.subscribe(() => this.modalService.fecharModal(this));
+  }
+
+}
