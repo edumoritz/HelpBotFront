42d734d9bd3d4c99f4034efa0dec5898bcab46ba
Joao <joao.pedro.bressan@hotmail.com>
2018-06-22 19:45:08 -0300

    Alteracoes gerais

--- src/app/features/modulo/modulo.module.ts
+++ src/app/features/modulo/modulo.module.ts
@@ -9,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { QuillModule } from 'ngx-quill';
 
 import { ModuloCadastroModalComponent } from './cadastro/modulo-cadastro-modal.component';
 import { ModuloVisualizacaoComponent } from './visualizacao/modulo-visualizacao.component';
@@ -26,7 +27,8 @@ import { ModalModule } from '../../components/modal/modal.module';
     MatButtonModule,
     MatAutocompleteModule,
     PaginationModule,
-    MatInputModule
+    MatInputModule,
+    QuillModule
   ],
   declarations: [
     ModuloVisualizacaoComponent,
